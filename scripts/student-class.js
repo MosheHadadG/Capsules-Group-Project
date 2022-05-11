8cda613bf043d748e6baffee87eb798cceb4d571
MosheHadadG <Moshehadad500@gmail.com>
2022-05-12 00:30:54 +0300

    Adding clean code

--- scripts/student-class.js
+++ scripts/student-class.js
@@ -1,116 +1,151 @@
-const students = document.querySelector('.students');
-const main = document.querySelector('main');
+// import { attributes } from "./constants/students.js";
 
+import { generateSutendetAttributes } from "./constants/students.js";
+
+const students = document.querySelector(".students");
+const main = document.querySelector("main");
+generateSutendetAttributes;
 export class Student {
   constructor(id, gender, firstName, lastName, hobby, age, city, capsule) {
-      this.id = id;
-      this.gender = gender;
-      this.firstName = firstName;
-      this.lastName = lastName;
-      this.hobby = hobby;
-      this.age = age;
-      this.city = city;
-      this.capsule = capsule;
-
+    this.id = id;
+    this.gender = translateGender(gender);
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.hobby = hobby;
+    this.age = age;
+    this.city = city;
+    this.capsule = capsule;
+    this.attributes = generateSutendetAttributes.call(this);
   }
 }
 
-Student.prototype.renderStudentToHtml = function() {
-  //* line
-  const line = document.createElement('div');
-  line.classList.add('line');
-  line.setAttribute("id", this.id);
-  
-  //* line id
-  const lineId = document.createElement('div');
-  lineId.classList.add('id');
-  lineId.innerText = this.id;
-  
 
-  //* line gender
-  this.gender = translateGender(this.gender)
-  const lineGender = document.createElement('input');
-  lineGender.classList.add('gender');
-  lineGender.placeholder = this.gender;
-  lineGender.setAttribute('disabled', 'disabled');
+Student.prototype.renderStudentToHtml = function () {
 
-  //* line first name
-  const lineFirst = document.createElement('input');
-  lineFirst.classList.add('first')
-  lineFirst.placeholder = this.firstName;
-  lineFirst.setAttribute('disabled', 'disabled');
+  const createNewCustomElement = (name, attr) => {
+    const el = document.createElement(name);
+    if (attr.length > 1) {
+      attr.forEach((keyValuePer) => {
+        el.setAttribute(keyValuePer[0], keyValuePer[1]);
+      });
+    } else {
+      el.setAttribute(attr[0][0], attr[0][1]);
+    }
 
-  //* line Last name
-  const linelast = document.createElement(`input`);
-  linelast.classList.add('last');
-  linelast.placeholder = this.lastName;
-  linelast.setAttribute('disabled', 'disabled');
+    return el;
+  };
 
-  //* line hobby
-  const lineHobby = document.createElement(`input`);
-  lineHobby.classList.add('hobby');
-  lineHobby.placeholder = this.hobby;
-  lineHobby.setAttribute('disabled', 'disabled');
-
-  // //* line age
-  const lineAge = document.createElement('input');
-  lineAge.classList.add('age');
-  lineAge.placeholder = this.age;
-  lineAge.setAttribute('disabled', 'disabled');
-
-  //* line city
-  const lineCity = document.createElement('input');
-  lineCity.classList.add('city');
-  lineCity.placeholder = this.city;
-  lineCity.setAttribute('disabled', 'disabled');
-
-  //* line capsule
-  const lineCapsule = document.createElement('input');
-  lineCapsule.classList.add('capsule');
-  lineCapsule.placeholder = this.capsule;
-  lineCapsule.setAttribute('disabled', 'disabled');
+  const { lineAttr, lineFirstAttr, lineGenderAttr, lineIdAttr, lineLastAtr,
+    lineHobbyAtr, lineAgeAtr, lineCityAtr, lineCapsuleAtr, lineEditAtr,
+    lineEditBtnAtr, lineDeleteAtr, lineDeleteBtnAtr } =
+    this.attributes;
 
 
-  //* line edit
-  const lineEdit = document.createElement('div');
-  lineEdit.classList.add('edit')
+  //! Create Elements
+  const line = createNewCustomElement("div", lineAttr);
+  const lineId = createNewCustomElement("div", lineIdAttr);
+  const lineGender = createNewCustomElement("input", lineGenderAttr);
+  const lineFirst = createNewCustomElement("input", lineFirstAttr);
+  const linelast = createNewCustomElement("input", lineLastAtr);
+  const lineHobby = createNewCustomElement("input", lineHobbyAtr);
+  const lineAge = createNewCustomElement("input", lineAgeAtr);
+  const lineCity = createNewCustomElement("input", lineCityAtr);
+  const lineCapsule = createNewCustomElement("input", lineCapsuleAtr);
+  const lineEdit = createNewCustomElement("div", lineCapsuleAtr);
+  const lineEditBtn = createNewCustomElement("button", lineEditBtnAtr);
+  const lineDelete = createNewCustomElement("div", lineDeleteAtr);
+  const lineDeleteBtn = createNewCustomElement("button", lineDeleteBtnAtr);
 
-  //*  line Button edit
-  const lineEditBtn = document.createElement('button');
-  lineEditBtn.classList.add('editBtn');
-  lineEditBtn.innerText = 'עריכה';
-  lineEditBtn.setAttribute("id", this.id);
+
+  const withoutLeading0 = parseInt(this.id, 10);
+  lineId.innerText = withoutLeading0;
+  this.gender = translateGender(this.gender);
+  lineEditBtn.innerText = "עריכה";
   lineEdit.appendChild(lineEditBtn);
+  lineDeleteBtn.innerText = "מחיקה";
+  lineDelete.appendChild(lineDeleteBtn);
 
+  let isEditing = "editing";
+  lineEditBtn.addEventListener("click", function pressEditButton() {
+    let inputs = line.querySelectorAll("input");
+    switch (isEditing) {
+      case "editing":
+        inputs.forEach((input) => {
+          input.removeAttribute("disabled");
+        });
+        isEditing = "approving";
+        line.classList.add("editline");
+        lineEditBtn.classList = "approv";
+        lineEditBtn.innerText = "אישור";
+        lineDeleteBtn.classList = "cancel";
+        lineDeleteBtn.innerText = "ביטול";
 
-  //*line Delete
-  const lineDelete = document.createElement('div');
-  lineDelete.classList.add('delete')
+        break;
+      case "approving":
+        inputs.forEach((input) => {
+          if (input.value !== "") {
+            input.placeholder = input.value;
+            input.value = "";
+          }
 
-  //* line button Delete
-  const lineDeleteBtn = document.createElement('button');
-  lineDeleteBtn.classList.add('deleteBtn');
-  lineDeleteBtn.innerText = 'מחיקה';
-  lineDelete.appendChild(lineDeleteBtn)
+          input.setAttribute("disabled", "disabled");
+          isEditing = "editing";
+        });
+        line.classList.remove("editline");
+        lineEditBtn.classList = "editBtn";
+        lineEditBtn.innerText = "עריכה";
+        lineDeleteBtn.classList = "deleteBtn";
+        lineDeleteBtn.innerText = "מחיקה";
 
-  
+        break;
+    }
+  });
+  lineDeleteBtn.addEventListener("click", function pressDeleteButton() {
+    let inputs = line.querySelectorAll("input");
+    switch (isEditing) {
+      case "editing":
+        line.remove();
+        break;
+
+      case "approving":
+        console.log("asdglhj");
+        inputs.forEach((input) => {
+          input.value = "";
+          input.setAttribute("disabled", "disabled");
+        });
+        isEditing = "editing";
+        line.classList.remove("editline");
+        lineEditBtn.classList = "editBtn";
+        lineEditBtn.innerText = "עריכה";
+        lineDeleteBtn.classList = "deleteBtn";
+        lineDeleteBtn.innerText = "מחיקה";
+        break;
+    }
+  });
   //! appends to line
-  line.appendChild(lineId);
-  line.appendChild(lineGender)
-  line.appendChild(lineFirst);
-  line.appendChild(linelast);
-  line.appendChild(lineHobby);
-  line.appendChild(lineAge);
-  line.appendChild(lineCity);
-  line.appendChild(lineCapsule);
-  line.appendChild(lineEdit)
-  line.appendChild(lineDelete);
+
+  const lineElements = [
+    lineId,
+    lineGender,
+    lineFirst,
+    linelast,
+    lineHobby,
+    lineAge,
+    lineCity,
+    lineCapsule,
+    lineEdit,
+    lineDelete,
+  ];
+
+  line.append(...lineElements);
 
   //! apeends To students
   main.appendChild(line);
-
-}
+};
 
 //* change word gender from engilsh to hebrew
-const translateGender = (gender) => gender === "male" ? "זכר" : "נקבה"
+const translateGender = (gender) => (gender === "male" ? "זכר" : "נקבה");
+  
+
+
 
