9b8470f5f78caca8c88ed087d63b4a91b02e6189
MosheHadadG <Moshehadad500@gmail.com>
2022-05-11 13:45:17 +0300

    add connect to api and get Students and create student

---
+++ scripts/get_data-api.js
@@ -0,0 +1,28 @@
+import { Student } from './student-class.js'
+async function getStudentes() {
+  const studentsUrl =  'https://capsules-asb6.herokuapp.com/api/teacher/toam'
+  const response = await fetch(studentsUrl);
+  const students = await response.json();
+  const studentIDs = students.map((student) => {
+    // console.log(student.id)
+    return student.id;
+  })
+  studentIDs.forEach((studentId) => {
+    getStudentCreateStudent(studentId)
+  })
+  // new student
+}
+
+getStudentes()
+
+async function getStudentCreateStudent(studentId) {
+  const studenUrl  = `https://capsules-asb6.herokuapp.com/api/user/${studentId}`;
+  const responseStudent = await fetch(studenUrl);
+  const student = await responseStudent.json();
+  // console.log(student)
+
+  const newStudent = new Student (student.id, student.gender, student.firstName,
+    student.lastName, student.hobby, student.age, student.city, student.capsule)
+  newStudent.renderStudentToHtml();
+  // console.log(newStudent)
+}
